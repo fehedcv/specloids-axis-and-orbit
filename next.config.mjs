915dd41ed444bf7745d30e22211d129d14c24717
Fahad <fahadshezao@gmail.com>
2026-01-03 22:35:10 +0530

    tried fix attempt 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Tells Next.js to generate static HTML
-  
+  assetPrefix: './',
+
+
   images: {
     unoptimized: true, // Required for static export
   },
